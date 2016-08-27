22173b3a8b1c2b5170283a91a0510db52a81e1bc
Chris Fraser <chris@zova.com>
2016-08-27 17:31:52 +1000

    Adds watchos target

---
+++ JWTDecode-watchOS/JWTDecode-watchOS.h
@@ -0,0 +1,19 @@
+//
+//  JWTDecode-watchOS.h
+//  JWTDecode-watchOS
+//
+//  Created by James Tonkin on 27/08/2016.
+//  Copyright © 2016 Auth0. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for JWTDecode-watchOS.
+FOUNDATION_EXPORT double JWTDecode_watchOSVersionNumber;
+
+//! Project version string for JWTDecode-watchOS.
+FOUNDATION_EXPORT const unsigned char JWTDecode_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JWTDecode_watchOS/PublicHeader.h>
+
+
